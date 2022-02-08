e4b88cd9bea46376ea50cf8dfcca724de7ad1757
Audino723 <rioaudino723@gmail.com>
2022-02-08 23:18:30 +0700

    damage_check

--- NgengNgengBrrr/src/main/bot/Bot.java
+++ NgengNgengBrrr/src/main/bot/Bot.java
@@ -41,47 +41,94 @@ public class Bot {
         List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block, gameState);
         List<Object> nextBlocks = blocks.subList(0,1);
 
-//        //Fix first if too damaged to move
-//        if(myCar.damage == 5) {
-//            return FIX;
-//        }
-//        //Accelerate first if going to slow
-//        if(myCar.speed <= 3) {
-//            return ACCELERATE;
-//        }
-//
-//        //Basic fix logic
-//        if(myCar.damage >= 5) {
-//            return FIX;
-//        }
-//
-//        //Basic avoidance logic
-//        if (blocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
-//            if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
-//                return LIZARD;
-//            }
-//            if (nextBlocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
-//                int i = random.nextInt(directionList.size());
-//                return directionList.get(i);
-//            }
-//        }
-//
-//        //Basic improvement logic
-//        if (hasPowerUp(PowerUps.BOOST, myCar.powerups)) {
-//            return BOOST;
-//        }
-//
-//        //Basic aggression logic
-//        if (myCar.speed == maxSpeed) {
-//            if (hasPowerUp(PowerUps.OIL, myCar.powerups)) {
-//                return OIL;
-//            }
-//            if (hasPowerUp(PowerUps.EMP, myCar.powerups)) {
-//                return EMP;
-//            }
-//        }
+        //Check damage
+        if (damage_check(myCar))
+        {
+            return FIX;
+        }
 
-        return TURN_RIGHT;
+        //Else accelerate
+        return ACCELERATE;
+        /*
+       //Fix first if too damaged to move
+       if(myCar.damage == 5) {
+           return FIX;
+       }
+       //Accelerate first if going to slow
+       if(myCar.speed <= 3) {
+           return ACCELERATE;
+       }
+
+       //Basic fix logic
+       if(myCar.damage >= 5) {
+           return FIX;
+       }
+
+       //Basic avoidance logic
+       if (blocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
+           if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
+               return LIZARD;
+           }
+           if (nextBlocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
+               int i = random.nextInt(directionList.size());
+               return directionList.get(i);
+           }
+       }
+
+       //Basic improvement logic
+       if (hasPowerUp(PowerUps.BOOST, myCar.powerups)) {
+           return BOOST;
+       }
+
+       //Basic aggression logic
+       if (myCar.speed == maxSpeed) {
+           if (hasPowerUp(PowerUps.OIL, myCar.powerups)) {
+               return OIL;
+           }
+           if (hasPowerUp(PowerUps.EMP, myCar.powerups)) {
+               return EMP;
+           }
+       }
+       */
+    }
+
+    private boolean damage_check(Car myCar){
+        int maxSpeed = max_speed_check(myCar);
+        if (myCar.speed == maxSpeed && (maxSpeed < 8 || (hasPowerUp(PowerUps.BOOST, myCar.powerups))))
+        {
+            return true;
+        }
+
+        return  false;
+    }
+
+    private int max_speed_check(Car myCar){
+        int maxSpeed;
+
+        switch(myCar.damage) {
+            case 5:
+                maxSpeed = 0;
+                break;
+            case 4:
+                maxSpeed = 3;
+                break;
+            case 3:
+                maxSpeed = 6;
+                break;
+            case 2:
+                maxSpeed = 8;
+                break;
+            case 1:
+                maxSpeed = 9;
+                break;
+            case 0:
+                maxSpeed = 15;
+                break;
+            default:
+                maxSpeed = 5;
+                break;
+        }
+        return  maxSpeed;
     }
 
     private Boolean hasPowerUp(PowerUps powerUpToCheck, PowerUps[] available) {
