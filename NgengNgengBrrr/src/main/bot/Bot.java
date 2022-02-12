ec7a6b7556e3cb7cd5a63d924eab9740e2a877cb
addinnabilal <16520342@std.stei.itb.ac.id>
2022-02-12 15:11:06 +0700

    fixing an error

--- NgengNgengBrrr/src/main/bot/Bot.java
+++ NgengNgengBrrr/src/main/bot/Bot.java
@@ -14,6 +14,7 @@ import static java.lang.Math.abs;
 public class Bot {
 
     private static final int maxSpeed = 9;
+    private List<Command> directionList = new ArrayList<>();
 
     private static final int MINIMUM_SPEED = 0;
     private static final int SPEED_STATE_1 = 3;
